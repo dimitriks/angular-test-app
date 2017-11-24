fa56edea65b8497219de40f80fc9bb839cc40463
Kucher Dmytro <dimitriks@gmail.com>
2017-11-24 16:31:56 +0200

    #43

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,7 +9,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply test', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
+    new Recipe('A Test Recipe', 'This is simply test', 'http://maxpixel.freegreatpicture.com/' +
+      'static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+    new Recipe('A Test Recipe', 'This is simply test', 'http://maxpixel.freegreatpicture.com/' +
+      'static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
   ];
 
   constructor() { }

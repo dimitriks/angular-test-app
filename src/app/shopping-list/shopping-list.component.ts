eba5e44b4d3c81efaef4be0d9d6032eb58a5065b
Kucher <kucher@buhphone.com>
2017-11-30 22:21:51 +0200

    193

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from "./shopping-list.service";
+import {Subject} from "rxjs/Subject";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private sLService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.sLService.getIngredients();
-    this.sLService.ingredientsChanged.subscribe(
+    this.subscription = this.sLService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onEditItem(index: number) {
+    this.sLService.startedEditing.next(index);
+
   }
 }

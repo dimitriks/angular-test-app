773f184d3d7350286627122ffa219f0d5f8ede98
Kucher <kucher@buhphone.com>
2017-12-05 11:40:29 +0200

    235

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {RecipeService} from "../recipes/recipe.service";
 import {HttpClient} from "@angular/common/http";
+import {Recipe} from "../recipes/recipe.model";
 
 @Injectable()
 export class DataStorageService {
@@ -10,4 +11,20 @@ export class DataStorageService {
     return this.http.put('https://angular-test-b2c05.firebaseio.com/recipes.json', this.recipeService.getRecipes());
   }
 
+  getRecipes() {
+    return this.http.get<Recipe[]>('https://angular-test-b2c05.firebaseio.com/recipes.json')
+      .subscribe(
+        (response) => {
+          const recipes: Recipe[] = response;
+
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              console.log(recipe);
+              recipe['ingredients'] = [];
+            }
+          }
+          this.recipeService.setRecipes(recipes);
+        }
+      );
+  }
 }
